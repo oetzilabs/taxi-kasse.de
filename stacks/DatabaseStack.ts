18dd8a05f4fcb32fdeb8c3d5d6403967ac2649ae
Özgür Isbert <oezguerisbert@gmail.com>
2023-09-17 13:18:27 +0200

    chores: RDS Aurora Scaling config

--- stacks/DatabaseStack.ts
+++ stacks/DatabaseStack.ts
@@ -4,6 +4,11 @@ export function DatabaseStack({ stack, app }: StackContext) {
   const db = new RDS(stack, "rds", {
     defaultDatabaseName: app.name,
     engine: "postgresql13.9",
+    scaling: {
+      autoPause: true,
+      minCapacity: "ACU_1",
+      maxCapacity: "ACU_2",
+    },
   });
 
   return {
