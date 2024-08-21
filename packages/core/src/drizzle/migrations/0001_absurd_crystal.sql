f5300eaddf184ad36de61f320fb46b98048f7f3a
Özgür Isbert <oezguerisbert@gmail.com>
2024-08-21 16:59:11 +0200

    cleanuo

--- packages/core/src/drizzle/migrations/0001_absurd_crystal.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `websockets` (
-	`id` text PRIMARY KEY NOT NULL,
-	`created_at` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
-	`updated_at` integer,
-	`deleted_at` integer,
-	`user_id` text,
-	`connection_id` text NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
-);
